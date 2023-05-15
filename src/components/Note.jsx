c118142948fbf2e667f10d933eb1c52d68982b2e
Matt Guardiola <matt.g.guardiola@gmail.com>
2023-05-15 18:44:53 -0500

    Site skeleton created

---
+++ src/components/Note.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+
+function Note() {
+    return <div className="note">
+        <h1> This is a Title</h1>
+        <p>This is some content</p>
+    </div>
+}
+
+export default Note;
