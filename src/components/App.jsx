81b9881fd9602bb0ab737c4859f9b5bb6f3a77cf
Matt Guardiola <matt.g.guardiola@gmail.com>
2023-05-18 16:58:40 -0500

    card components can now update dynamically based on objects in the notes array

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,12 +1,22 @@
 import Header from "./Header";
 import Footer from "./Footer";
-import Note from "./Note";
+import Card from "./Card";
+import notes from "../notes"
+
+function displayCard(note){
+  return <Card 
+  key={note.id}
+  title={note.title}
+  content={note.content}
+  />
+}
+
 
 function App() {
   return (
     <div className="App">
     <Header />
-    <Note />
+    {notes.map(displayCard)}
     <Footer />
     </div>
   );
